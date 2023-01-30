9abbccbdfc31cfcac1304c9d498271aba57701fd
이채하 <chaeha93@gmail.com>
2023-01-31 00:54:17 +0900

    style: Initial package settings

---
+++ backend/clog/src/main/java/com/clog/ClogApplication.java
@@ -0,0 +1,13 @@
+package com.clog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClogApplication.class, args);
+    }
+
+}
